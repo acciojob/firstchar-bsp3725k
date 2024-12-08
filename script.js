861e0f6ae7b271b9b844a4c09de210e7183f4e2e
bsp3725k <139534037+bsp3725k@users.noreply.github.com>
2024-12-08 15:21:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function firstChar(text) {
   // your code here
 	let str = text.trim();
-	return str[0];
+	return str.charAt(0);
 } 
 // Do not change the code below
 //Uncomment the following line to show the prompt popup
